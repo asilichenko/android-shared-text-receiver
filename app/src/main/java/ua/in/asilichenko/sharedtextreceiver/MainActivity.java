0b6bfc6080978921b46d06df6e8598cf06b3ba09
Oleksii Sylichenko <a.silichenko@gmail.com>
2023-09-18 14:58:53 +0300

    initial commit

---
+++ app/src/main/java/ua/in/asilichenko/sharedtextreceiver/MainActivity.java
@@ -0,0 +1,59 @@
+package ua.in.asilichenko.sharedtextreceiver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = "Share Text Receiver";
+
+  private TextView logTextView;
+
+  private void log(String text) {
+    Log.d(TAG, text);
+    logTextView.append(text + "\n\n");
+  }
+
+  private void receiveText() {
+    final Intent intent = getIntent();
+    final String action = intent.getAction();
+    if (Intent.ACTION_SEND.equals(action)) {
+      log("Intent String Extra: " + intent.getStringExtra(Intent.EXTRA_TEXT));
+    } else {
+      log("Intent Action: " + action);
+    }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    logTextView = findViewById(R.id.logTextView);
+    log("=================== onCreate ===================");
+    receiveText();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    log("onPause");
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    log("onNewIntent");
+    setIntent(intent);
+    receiveText();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    log("onResume");
+  }
+}
